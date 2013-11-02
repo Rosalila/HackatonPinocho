82dc9c21b327ccb116a7954a7b3d897b4188bfc4
Oscar Rene Presidente <oscar.rene1989@gmail.com>
2013-11-02 15:15:10 -0600

    Added Game Constants

--- src/rosalila/studio/hackatonpinocho/GameActivity.java
+++ src/rosalila/studio/hackatonpinocho/GameActivity.java
@@ -1,23 +1,37 @@
 package rosalila.studio.hackatonpinocho;
 
+import org.andengine.engine.camera.Camera;
 import org.andengine.engine.options.EngineOptions;
+import org.andengine.engine.options.ScreenOrientation;
+import org.andengine.engine.options.resolutionpolicy.FillResolutionPolicy;
 import org.andengine.entity.scene.Scene;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
+import org.andengine.opengl.texture.atlas.bitmap.BuildableBitmapTextureAtlas;
+import org.andengine.opengl.texture.region.ITextureRegion;
 import org.andengine.ui.activity.BaseGameActivity;
 
 public class GameActivity extends BaseGameActivity {
 
+	private Camera mCamera;
+	
+	private ITextureRegion mSplashBackground;
+	
 	@Override
 	public EngineOptions onCreateEngineOptions() {
-		// TODO Auto-generated method stub
-		return null;
+        mCamera = new Camera(0, 0, GameConstants.CAMERA_WIDTH, GameConstants.CAMERA_HEIGHT);
+		EngineOptions options = new EngineOptions(true, ScreenOrientation.LANDSCAPE_SENSOR, new FillResolutionPolicy(), mCamera);
+		
+		return options;
 	}
 
 	@Override
 	public void onCreateResources(
 			OnCreateResourcesCallback pOnCreateResourcesCallback)
 			throws Exception {
-		// TODO Auto-generated method stub
-
+		BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx");
+		
+		BuildableBitmapTextureAtlas splashTextureAtlas = new BuildableBitmapTextureAtlas(getTextureManager(), GameConstants.CAMERA_WIDTH, GameConstants.CAMERA_HEIGHT);
+		mSplashBackground = BitmapTextureAtlasTextureRegionFactory.createFromAsset(splashTextureAtlas, this, "rosalila_logo.png");
 	}
 
 	@Override
---
+++ src/rosalila/studio/hackatonpinocho/GameConstants.java
@@ -0,0 +1,7 @@
+package rosalila.studio.hackatonpinocho;
+
+public class GameConstants {
+
+	public static final int CAMERA_HEIGHT = 1280;
+	public static final int CAMERA_WIDTH = 720;
+}
